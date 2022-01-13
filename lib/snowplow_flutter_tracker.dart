5283bc45fc8d4723cb8cff9be5cb5b9920f03d7c
Matúš Tomlein <matus.tomlein@gmail.com>
2022-01-13 10:53:18 +0100

    Add license headers to source files

--- lib/snowplow_flutter_tracker.dart
+++ lib/snowplow_flutter_tracker.dart
@@ -9,28 +9,5 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 
-@JS()
-library snowplow;
-
-import 'package:js/js.dart';
-
-@JS('window.snowplow')
-external void snowplow(String method,
-    [dynamic arg1,
-    dynamic arg2,
-    dynamic arg3,
-    dynamic arg4,
-    dynamic arg5,
-    dynamic arg6]);
-
-@JS('addSessionContextPlugin')
-external void addSessionContextPlugin(String tracker);
-
-@JS('getSnowplowDuid')
-external String? getSnowplowDuid();
-
-@JS('getSnowplowSid')
-external String? getSnowplowSid();
-
-@JS('getSnowplowVid')
-external int? getSnowplowVid();
+export 'snowplow.dart';
+export 'tracker.dart';
