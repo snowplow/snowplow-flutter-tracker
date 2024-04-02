ce1f36e6598e35256730e52a9edacc8477be6452
Matúš Tomlein <matus.tomlein@gmail.com>
2024-04-02 15:18:38 +0200

    Run media integration tests

--- example/tool/e2e_tests.sh
+++ example/tool/e2e_tests.sh
@@ -6,3 +6,4 @@ set -e
 flutter drive --driver test_driver/integration_test.dart --target integration_test/configuration_test.dart --dart-define=ENDPOINT=$1 $2
 flutter drive --driver test_driver/integration_test.dart --target integration_test/events_test.dart --dart-define=ENDPOINT=$1 $2
 flutter drive --driver test_driver/integration_test.dart --target integration_test/session_test.dart --dart-define=ENDPOINT=$1 $2
+flutter drive --driver test_driver/integration_test.dart --target integration_test/media_test.dart --dart-define=ENDPOINT=$1 $2
