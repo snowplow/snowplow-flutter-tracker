a466b0a9a7478392a53bbd940f31a679a42c387f
Jack Keene <jack.keene@snowplowanalytics.com>
2026-03-24 15:03:09 +0000

    fix failing tests

--- example/integration_test/events_test.dart
+++ example/integration_test/events_test.dart
@@ -10,7 +10,6 @@
 // See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:snowplow_tracker/snowplow_tracker.dart';
 import 'package:uuid/uuid.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -256,17 +255,13 @@ void main() {
         isTrue);
   });
 
-  testWidgets("raises an exception when tracking page view event on mobile",
+  testWidgets("tracks page view event on mobile",
       (WidgetTester tester) async {
     if (kIsWeb) {
       return;
     }
 
-    try {
-      await Snowplow.track(const PageViewEvent(), tracker: 'test');
-      fail('Exception not thrown');
-    } catch (e) {
-      expect(e, isInstanceOf<MissingPluginException>());
-    }
+    // PageView is now supported on mobile — should not throw
+    await Snowplow.track(const PageViewEvent(), tracker: 'test');
   });
 }
